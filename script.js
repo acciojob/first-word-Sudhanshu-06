fe794d9c6c2e35edbf273d457cb2f965fad38d63
Sudhanshu Kumar <114154455+Sudhanshu-06@users.noreply.github.com>
2024-03-04 10:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	if(s.length===0){
+		return '';
+	}
+	let words=s.split(' ');
+	return words[0];
 }
+
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
